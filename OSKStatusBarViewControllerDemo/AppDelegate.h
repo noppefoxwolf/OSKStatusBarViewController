30f2b3dfd3922db39bb0c2958f8cf3dbd1a6b09d
hirano_tomoya <cromteria@gmail.com>
2015-05-23 05:01:54 +0900

    Initial Commit

---
+++ OSKStatusBarViewControllerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OSKStatusBarViewControllerDemo
+//
+//  Created by Tomoya_Hirano on 5/23/15.
+//  Copyright (c) 2015 Tomoya_Hirano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OSKStatusBarViewControllerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OSKStatusBarViewControllerDemo
+//
+//  Created by Tomoya_Hirano on 5/23/15.
+//  Copyright (c) 2015 Tomoya_Hirano. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
