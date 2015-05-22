7acade0866c0464b1607abcf07878d6997f4b15d
hirano_tomoya <cromteria@gmail.com>
2015-05-23 05:06:12 +0900

    fix

--- OSKStatusBarViewControllerDemo/ViewController.m
+++ OSKStatusBarViewControllerDemo/ViewController.m
@@ -17,7 +17,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.statusBarColor = [UIColor redColor];
+    self.statusBarColor = [self randomColor];
+}
+
+- (UIColor*)randomColor{
+    CGFloat r = (arc4random_uniform(255) + 1)/255.0;
+    CGFloat g = (arc4random_uniform(255) + 1)/255.0;
+    CGFloat b = (arc4random_uniform(255) + 1)/255.0;
+    return [UIColor colorWithRed:r green:g blue:b alpha:1.0];
 }
 
 - (void)didReceiveMemoryWarning {
